f3b074aaba07983f22f3616b3f39b2f3690f7c3a
German Montenegro <gnm3000@gmail.com>
2024-08-12 20:11:14 -0300

    adding logging and cache

--- starships-app/app.py
+++ starships-app/app.py
@@ -1,10 +1,13 @@
 from chalice import Chalice, AuthResponse, UnauthorizedError
 from chalicelib import auth
 from chalicelib.swapidev import Swapidev
+import logging
 
 app = Chalice(app_name="starships-app")
 app.debug = False
 users_db = {"user1": "12345"}
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route("/login", methods=["POST"], cors=True)
 def login():
@@ -14,7 +17,7 @@ def login():
 
     if username in users_db and users_db[username] == password:
         token = auth.get_jwt_token(username)
-        print(f"Generated JWT for {username}: {token}")  # Debugging line
+        logger.info(f"Generated JWT for {username}: {token}") 
         return {"token": token}
     else:
         raise UnauthorizedError("Invalid username or password")
@@ -22,7 +25,6 @@ def login():
 @app.authorizer()
 def jwt_auth(auth_request):
     token = auth_request.token
-    print(f"Received token: {token}")  # Debugging line
     if not token:
         raise UnauthorizedError("No token provided")
     if token.startswith("Bearer "):
@@ -31,21 +33,19 @@ def jwt_auth(auth_request):
     decoded = auth.decode_jwt_token(token)
     if decoded is None:
         raise UnauthorizedError("Invalid token")
-    print(f"Decoded token: {decoded}")  # Debugging line
+    
     return AuthResponse(routes=["*"], principal_id=decoded["sub"])
 
 @app.route("/starships", methods=["GET"], authorizer=jwt_auth, cors=True)
 def get_starships():
-    params = app.current_request.query_params
-    manufacturer = None
-    if params and "manufacturer" in params:
-        manufacturer = params["manufacturer"]
+    params = app.current_request.query_params or {}
     swapidev = Swapidev()
-    starships = swapidev.get_starships(manufacturer=manufacturer)
+    starships = swapidev.get_starships(**params)
     return {"starships": starships}
 
 @app.route("/manufacturers", methods=["GET"], authorizer=jwt_auth, cors=True)
 def get_manufacturer():
+    
     swapidev = Swapidev()
     manufacturers = swapidev.get_manufacturers()
     return {"manufacturers": manufacturers}
--- starships-app/chalicelib/swapidev.py
+++ starships-app/chalicelib/swapidev.py
@@ -1,20 +1,41 @@
+import time
 import requests
-
+from cacheout import Cache
+import logging
+# Obtener el logger ya configurado
+logger = logging.getLogger(__name__)
 
 class Swapidev:
+    
+    cache = Cache(maxsize=256, ttl=300, timer=time.time, default=None)
+    # class variable
+
     def __init__(self) -> None:
         pass
 
-    def get_starships(self, manufacturer=None):
+    def get_starships(self, **filters):
+        logger.info("Fetching starships with filters: %s", filters)
         response = requests.get("https://swapi.dev/api/starships/")
+        #we get json from API
         starships = response.json().get("results", [])
-        if manufacturer:
-            starships = (s for s in starships if manufacturer == s["manufacturer"])
-        return list(starships)
+        lambda_function = lambda x: all(x.get(key) == value for key,value in filters.items())
+        # all() return true if  our object gets true for all filters
+        filtered = filter(lambda_function,starships)
+
+        return list(filtered)
 
     def get_manufacturers(self):
+        
+        #return form cache if exists
+        if 'manufacturers' in self.cache:
+            logger.info("Cache hit: Retuen manufacturers from cache")
+            return self.cache.get('manufacturers')
+        logger.info("Cache API hit: Fetching manufacturers from API")
         response = requests.get("https://swapi.dev/api/starships/")
         starships = response.json().get("results", [])
-        manufacturers = set(s["manufacturer"] for s in starships)
-        return list(manufacturers)
+        manufacturers = {s["manufacturer"] for s in starships}
+        manufacturers_list = list(manufacturers)
 
+        self.cache.set('manufacturers', manufacturers_list)
+        return manufacturers_list
+    
